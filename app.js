3b06779b3d5a4b92460eccf479c46c8a660f9aec
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-12 16:22:16 +0000

    Adjusted fahrenheit from const to let for exercise

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const kelvin = 293;
 // Convert Kelvin to Celsius by subtracting 273 from the kelvin variable
 const celsius = kelvin - 273;
 // Calculates fahrenheit from celsius
-const fahrenheit = celsius * (9/5) + 32;
+let fahrenheit = celsius * (9/5) + 32;
